402f8fd5a3cd415997e40fba3dc7628aedee4255
vladislav-o0 <vladislavw231@gmail.com>
2021-04-21 13:49:40 +0300

    added styles form upload

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,11 +1,12 @@
 <template>
-    <form @submit.prevent="upload" id="uploadForm" enctype="multipart/form-data">
+    <form class="uploadForm" @submit.prevent="upload" id="uploadForm" enctype="multipart/form-data">
         <input type="text" name="author" placeholder="Автор">
         <input type="text" name="name" placeholder="Название">
         <select name="genre">
             <option v-for="(value, key) in genres" :value="key">{{value}}</option>
         </select>
-        <input type="file" name="file">
+        <input @change="selectedFile" id="uploadFile" hidden type="file" name="file">
+        <label class="uploadFile" for="uploadFile">{{fileName}}</label>
         <input type="submit" value="Отправить">
     </form>
 </template>
@@ -14,8 +15,19 @@
     import { mapState } from 'vuex';
 
     export default {
+        data() {
+            return {
+                fileName: 'Выберите аудио'
+            }
+        },
         computed: mapState(['genres']),
         methods: {
+            selectedFile(e) {
+                let files = e.target.files;
+
+                if (!files.length) return this.fileName = 'Выберите аудио';
+                this.fileName = files[0].name;
+            },
             upload() {
                 let form = document.getElementById('uploadForm');
                 let formData = new FormData(form);
@@ -34,6 +46,18 @@
     }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+    .uploadForm {
+        display: flex;
+        flex-flow: column;
+        max-width: 350px;
+    }
+    input, select, .uploadFile {
+    border-radius: 5px;
+    height: 30px;
+    border: 2px solid #85919c;
+    }
+    .uploadFile {
+        background-color: white;
+    }
 </style>
